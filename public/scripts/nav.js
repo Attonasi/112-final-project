9ebe63bcb37be3c9627e640ad2db06d7e5f0faa3
Attonasi <attonasi@hotmail.com>
2017-06-04 12:24:44 -0700

    late night stuff

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -2,14 +2,40 @@
 
 $(function () {
 
-    $('.row.mobileHamburgerNav').click(function () {
+    var $current = `.homePage`;
 
-        console.log('clicked');
+    $('.row.mobileHamburgerNav').click(function () {
         $('#navMenu').fadeToggle();
 
     });
 
+    $(`.homeLink`).click(function (event) {
+        console.log($current, event.target);
+        $($current).fadeOut('slow');
+        $current = `.homePage`;
+        $(`.homePage`).fadeIn('slow');
+    })
 
+    $(`.accountLink`).click(function (event) {
+        console.log($current, event.target);
+        $($current).fadeOut('slow');
+        $current = `.accountPage`;
+        $(`.accountPage`).fadeIn('slow');
+    })
+
+    $(`.profileLink`).click(function (event) {
+        console.log($current, event.target);
+        $($current).fadeOut('slow');
+        $current = `.profilePage`;
+        $(`.profilePage`).fadeIn('slow');
+    })
+
+    $(`.contactLink`).click(function (event) {
+        console.log($current, event.target);
+        $($current).fadeOut('slow');
+        $current = `.contactPage`;
+        $(`.contactPage`).fadeIn('slow');
+    })
 
 
 
