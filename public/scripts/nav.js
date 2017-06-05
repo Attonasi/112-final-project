f5da6263c089d619908ade56a7925266dc60f975
Attonasi <attonasi@hotmail.com>
2017-06-04 23:34:12 -0700

    commit

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,41 +3,52 @@
 $(function () {
 
 
-
+    //    current page variable
     var $current = `.homePage`;
 
+    //    toggles the hamburger menu on the mobile display, the hamburger is actually a lion
     $('.row.mobileHamburgerNav').click(function () {
         $('#navMenu').fadeToggle();
-
     });
 
+    //    These are the navigation link classes. any object with these classes becomes a "link" to that page.
     $(`.homeLink`).click(function (event) {
-        console.log($current, event.target);
+
         $($current).fadeOut('slow');
         $current = `.homePage`;
         $(`.homePage`).fadeIn('slow');
-    })
+    });
 
     $(`.accountLink`).click(function (event) {
-        console.log($current, event.target);
+
         $($current).fadeOut('slow');
         $current = `.accountPage`;
         $(`.accountPage`).fadeIn('slow');
-    })
+    });
 
     $(`.profileLink`).click(function (event) {
-        console.log($current, event.target);
+
         $($current).fadeOut('slow');
         $current = `.profilePage`;
         $(`.profilePage`).fadeIn('slow');
-    })
+    });
 
     $(`.contactLink`).click(function (event) {
-        console.log($current, event.target);
+
         $($current).fadeOut('slow');
         $current = `.contactPage`;
         $(`.contactPage`).fadeIn('slow');
-    })
+    });
+
+    //    End Page Class Links
+
+    //    Toggle for the login page forms
+    $('.message').click(function () {
+        $('form').animate({
+            height: 'toggle',
+            opacity: 'toggle'
+        }, 'slow');
+    });
 
 
     console.log('arg', $current);
