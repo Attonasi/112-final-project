1445b576ca441a82a90e0e520bbd5db65e3a793c
Attonasi <attonasi@hotmail.com>
2017-06-06 12:16:18 -0700

    almost mvp

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -13,9 +13,9 @@ $(function () {
 
 
     $('#bargleCollinsProfile').val('Collin Smith');
-    $('#titleCollinsProfie').val('Training Instructor');
+    $('#titleCollinsProfile').val('Training Instructor');
     $('#phoneCollinsProfile').val('(425)736-5063');
     $('#emailCollinsProfile').val('attonasi@hotmai.com');
 
-    $('#goalsCollinsProfile').val('I have been active and competing in athletics for over 30 years. I have had several jobs, started several businesses, joined the Army, and am the father of two daughters. I know how hard it is to find time to stay in shape.<br> I have been able to develop training methods that allow me to maintain a high level of fitness with very limited time. I can show you how to do this too.');
+    $('#goalsCollinsProfile').val('I have been active and competing in athletics for over 30 years. I have had several jobs, started several businesses, joined the Army, and am the father of two daughters. I know how hard it is to find time to stay in shape. I have been able to develop training methods that allow me to maintain a high level of fitness with very limited time. I can show you how to do this too.');
 });
