981d3a4ebd8b38a19488f9d12b220013680c35a6
Attonasi <attonasi@hotmail.com>
2017-06-06 15:44:04 -0700

    really almost mvp

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -6,57 +6,13 @@ $(function () {
     //    current page variable
     var $current = `.homePage`;
 
+    //    complete navigation system for the SPA here
+
     $('.navigationLink').click(function (event) {
         $($current).fadeOut('fast');
         $(`.${event.target.id}Page`).fadeIn('slow');
         $current = `.${event.target.id}Page`;
-        console.log(`.${event.target.id}Page`, $current, `.${event.target.id}`);
     })
-
-
-    //    These are the navigation link classes. any object with these classes becomes a "link" to that page.
-//    $(`.homeLink`).click(function (event) {
-            //
-            //        $($current).fadeOut('slow');
-            //        $current = `.homePage`;
-            //        $(`.homePage`).fadeIn('slow');
-            //        console.log(event.target.className, $current);
-            //    });
-            //
-            //    $(`.accountLink`).click(function (event) {
-            //
-            //        $($current).fadeOut('slow');
-            //        $current = `.accountPage`;
-            //        $(`.accountPage`).fadeIn('slow');
-            //        console.log(event.target.className, $current);
-            //    });
-            //
-            //    $(`.profileLink`).click(function (event) {
-            //
-            //        $($current).fadeOut('slow');
-            //        $current = `.profilePage`;
-            //        $(`.profilePage`).fadeIn('slow');
-            //        console.log(event.target.className, $current);
-            //    });
-            //
-            //    $(`.contactLink`).click(function (event) {
-            //
-            //        $($current).fadeOut('slow');
-            //        $current = `.contactPage`;
-            //        $(`.contactPage`).fadeIn('slow');
-            //        console.log(event.target.className, $current);
-            //    });
-            //
-            //    //    End Page Class Links
-            //
-            //    //    Toggle for the login page forms
-            //    $('.message a').click(function () {
-            //        $('form').animate({
-            //            height: 'toggle',
-            //            opacity: 'toggle'
-            //        }, 'slow');
-            //    });
-
 
     console.log($current);
 
